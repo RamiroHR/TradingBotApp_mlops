0ff9fbdd245c1f5febbb911537e968c391028f25
RamiroHR <19.rodriguez.89@gmail.com>
2024-01-11 15:27:34 +0100

    Initial DS template from coockiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Trading Bot App as MLOps project',
+    author='RAS_mlops_oct23',
+    license='MIT',
+)
